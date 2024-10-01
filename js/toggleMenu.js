809a6f90cd75a853963ee3eb6bd56ce566e06a09
xtpen <bilogmanuel.educ@gmail.com>
2024-10-01 15:50:08 +0800

    first push burgelo

---
+++ js/toggleMenu.js
@@ -0,0 +1,5 @@
+const burgir = document.querySelector(".toggle__menu");
+const headerNav = document.querySelector("ul.header__nav");
+burgir.addEventListener("click", () => {
+  headerNav.classList.toggle("open")
+})
